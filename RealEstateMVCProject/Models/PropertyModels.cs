31d7361a22ff91ef29793e5e9df1a83eb4a7faf2
Your Name <you.example.com>
2016-05-16 20:06:52 -0400

    Submitting RealEstateMVCProject

---
+++ RealEstateMVCProject/Models/PropertyModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RealEstateMVCProject.Models
+{
+    public class PropertyModels
+    {
+        [Key]
+        public int PropertyID { get; set; }
+        public string Title { get; set; }
+        public string ImageSource { get; set; }
+        public string Description { get; set; }
+
+       
+
+        /*images, height, overlays, description, content
+        * button to link to learn more
+        * Title*/
+
+
+
+    }
+}
